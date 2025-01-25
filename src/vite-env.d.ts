5b8056139b3a2842d6dabf4aec86eec76d1427cd
chenguolong <1582576318@qq.com>
2025-01-25 19:32:09 +0800

    style: 重新配置eslint、prettierrc、ts、vite

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string // 根据你在 `.env` 文件中的变量定义
+  readonly VITE_APP_TITLE: string
+  // 添加更多环境变量...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,38 @@
-import type { UserConfigExport } from 'vite'
 import { resolve } from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
+import { defineConfig, loadEnv } from 'vite'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
-import svgLoader from "vite-svg-loader"
+import svgLoader from 'vite-svg-loader'
 
 // https://vite.dev/config/
-export default (): UserConfigExport => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd()) as ImportMetaEnv
+  console.log(env)
   return {
     plugins: [
       vue(),
       AutoImport({
-        resolvers: [ElementPlusResolver()],
+        resolvers: [ElementPlusResolver()]
       }),
       Components({
-        resolvers: [ElementPlusResolver()],
+        resolvers: [ElementPlusResolver()]
       }),
       /** 将 SVG 静态图转化为 Vue 组件 */
-      svgLoader({ defaultImport: "url" }),
+      svgLoader({ defaultImport: 'url' }),
       createSvgIconsPlugin({
         // Specify the icon folder to be cached
         iconDirs: [resolve(process.cwd(), 'src/icons/svg')],
         // Specify symbolId format
-        symbolId: 'icon-[dir]-[name]',
-      }),
+        symbolId: 'icon-[dir]-[name]'
+      })
     ],
     resolve: {
       alias: {
-        '@': resolve(__dirname, 'src'),
-      },
-    },
+        '@': resolve(__dirname, 'src')
+      }
+    }
   }
-}
+})
